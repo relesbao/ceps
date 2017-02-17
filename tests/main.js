b285154a95d5a5fe6a18045fd38ae26ac35bc250
Wanderson <wandersonwhcr@gmail.com>
2017-02-17 10:14:58 -0200

    Cria teste unitário para verificar arquivos

--- tests/main.js
+++ tests/main.js
@@ -1,5 +1,11 @@
 fs = require('fs');
 
+exports['test is file'] = function (assert) {
+    fs.readdirSync('data', {}).forEach(function (filename) {
+        assert.ok(fs.lstatSync('data/' + filename).isFile(), filename);
+    });
+};
+
 exports['test filenames'] = function (assert) {
     fs.readdirSync('data', {}).forEach(function (filename) {
         assert.ok(/^[0-9]{8}\.json$/.test(filename), filename);
