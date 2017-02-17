234983ae9c168f6554fa6fcfd8d4d905ff77dda8
Wanderson <wandersonwhcr@gmail.com>
2017-02-17 10:28:16 -0200

    Inicializa verificação do conteúdo do arquivo

--- tests/main.js
+++ tests/main.js
@@ -6,9 +6,22 @@ exports['test is file'] = function (assert) {
     });
 };
 
-exports['test filenames'] = function (assert) {
+exports['test filename'] = function (assert) {
     fs.readdirSync('data', {}).forEach(function (filename) {
         assert.ok(/^[0-9]{8}\.json$/.test(filename), filename);
+    });
+};
+
+exports['test content'] = function (assert) {
+    fs.readdirSync('data', {}).forEach(function (filename) {
+        // Carrega JSON
+        var element = JSON.parse(fs.readFileSync('data/' + filename, 'utf8'));
+
+        // Verificações
+
+        assert.equal(typeof element._id, 'string', filename + ' "_id" exists');
+        assert.ok(/^[0-9]{8}$/.test(element._id), filename + ' "_id" has correct pattern');
+        assert.equal(filename.substring(0, 8), element._id, filename + ' "_id" is equal to filename');
     });
 };
 
