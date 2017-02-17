96bb86837c05bb7f72b091fdbc824cd3d8c7aca4
Wanderson <wandersonwhcr@gmail.com>
2017-02-17 10:37:11 -0200

    Verifica se tipo do logradouro é válido

--- tests/main.js
+++ tests/main.js
@@ -1,5 +1,52 @@
 fs = require('fs');
 
+validTypes = [
+    'Aeroporto',
+    'Alameda',
+    'Área',
+    'Avenida',
+    'Campo',
+    'Chácara',
+    'Colônia',
+    'Condomínio',
+    'Conjunto',
+    'Distrito',
+    'Esplanada',
+    'Estação',
+    'Estrada',
+    'Favela',
+    'Fazenda',
+    'Feira',
+    'Jardim',
+    'Ladeira',
+    'Lago',
+    'Lagoa',
+    'Largo',
+    'Loteamento',
+    'Morro',
+    'Núcleo',
+    'Parque',
+    'Passarela',
+    'Pátio',
+    'Praça',
+    'Quadra',
+    'Recanto',
+    'Residencial',
+    'Rodovia',
+    'Rua',
+    'Setor',
+    'Sítio',
+    'Travessa',
+    'Trecho',
+    'Trevo',
+    'Vale',
+    'Vereda',
+    'Via',
+    'Viaduto',
+    'Viela',
+    'Vila',
+];
+
 exports['test is file'] = function (assert) {
     fs.readdirSync('data', {}).forEach(function (filename) {
         assert.ok(fs.lstatSync('data/' + filename).isFile(), filename);
@@ -20,8 +67,11 @@ exports['test content'] = function (assert) {
         // Verificações
 
         assert.equal(typeof element._id, 'string', filename + ' "_id" exists');
-        assert.ok(/^[0-9]{8}$/.test(element._id), filename + ' "_id" has correct pattern');
+        assert.ok(/^[0-9]{8}$/.test(element._id), filename + ' "_id" is valid');
         assert.equal(filename.substring(0, 8), element._id, filename + ' "_id" is equal to filename');
+
+        assert.equal(typeof element.tipo, 'string', filename + ' "tipo" exists');
+        assert.ok(validTypes.indexOf(element.tipo) >= 0, filename + ' "tipo" is valid');
     });
 };
 
